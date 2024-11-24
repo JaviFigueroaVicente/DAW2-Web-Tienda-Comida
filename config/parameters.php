dc435bb1b578318837cf5a0f14b974f4373a39c9
Javi Figueroa <javierfigueroa969@gmail.com>
2024-11-24 22:35:26 +0100

    general

--- config/parameters.php
+++
@@ -1,3 +0,0 @@
-<?php
-define("default_aciton", "index");
-?>
--- controllers/indexController.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class IndexController {
-
-        public function index() {            
-            include_once "views/index.php"; 
-        }
-    }
-?>
